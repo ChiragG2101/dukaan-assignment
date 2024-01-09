a1b78bc572f56688a37cdf62a23ff518813c03fd
ChiragG2101 <chiragg593@gmail.com>
2024-01-10 01:23:44 +0530

    folder restructuring changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import Others from "../src/components/pages/Others";
+import Others from "./pages/Others";
 import { sidebarMenuOptions } from "./lib/sidebar-items";
-import Payments from "../src/components/pages/Payments";
+import Payments from "./pages/Payments";
 
 function App() {
   return (
